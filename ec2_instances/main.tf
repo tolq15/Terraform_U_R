38aac803df025f603278343605b2a27bce45dc4a
Anatoli Lyssak <tolq15@gmail.com>
2020-05-19 08:34:10 -0600

    Create several EC2 instances at once

---
+++ ec2_instances/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region  = "us-east-2"
+  version = "~> 2.60"
+}
+
+resource "aws_key_pair" "lt-demo" {
+  key_name   = "lt-demo"
+  public_key = file("id_rsa.pub")
+}
+
+resource "aws_instance" "lt-instance" {
+  count         = var.instance_count
+  ami           = lookup(var.ami,var.aws_region)
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.lt-demo.key_name
+
+  vpc_security_group_ids = [aws_security_group.lt_sg_01.id]
+
+tags = {
+    Name  = "U-R-0${count.index + 1}"
+    Batch = "5AM"
+  }
+
+}
+
+resource "aws_security_group" "lt_sg_01" {
+
+  name = "U_R_01_sg"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
---
+++ ec2_instances/variables.tf
@@ -0,0 +1,27 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 22
+}
+
+variable "ami" {
+  type = map
+
+  default = {
+    "us-east-1" = "ami-07c1207a9d40bc3bd"
+    "us-east-2" = "ami-07c1207a9d40bc3bd"
+    "us-west-1" = "ami-07c1207a9d40bc3bd"
+  }
+}
+
+variable "instance_count" {
+  default = "4"
+}
+
+variable "instance_type" {
+  default = "t2.nano"
+}
+
+variable "aws_region" {
+  default = "us-east-2"
+}
